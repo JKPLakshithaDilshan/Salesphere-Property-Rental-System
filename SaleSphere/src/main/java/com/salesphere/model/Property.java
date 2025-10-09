aa0e5a9d24b8883a9d26f892dda8057d3d83faff
Lakshitha Dilshan <info.jkplakshitha@gmail.com>
2025-10-09 15:51:25 +0530

    Update Property.java

--- SaleSphere/src/main/java/com/salesphere/model/Property.java
+++ SaleSphere/src/main/java/com/salesphere/model/Property.java
@@ -14,6 +14,14 @@ public class Property {
 	private String landlordName;
 	private String landlordEmail;
 	
+	// Image management fields
+	private String mainImage;
+	private String image1;
+	private String image2;
+	private String image3;
+	private String image4;
+	private String image5;
+	
 	public int getPropertyId() {
 		return propertyId;
 	}
@@ -79,5 +87,45 @@ public class Property {
 	}
 	public void setLandlordEmail(String landlordEmail) {
 		this.landlordEmail = landlordEmail;
+	}
+	
+	// Image management getters
+	public String getMainImage() {
+		return mainImage;
+	}
+	public String getImage1() {
+		return image1;
+	}
+	public String getImage2() {
+		return image2;
+	}
+	public String getImage3() {
+		return image3;
+	}
+	public String getImage4() {
+		return image4;
+	}
+	public String getImage5() {
+		return image5;
+	}
+	
+	// Image management setters
+	public void setMainImage(String mainImage) {
+		this.mainImage = mainImage;
+	}
+	public void setImage1(String image1) {
+		this.image1 = image1;
+	}
+	public void setImage2(String image2) {
+		this.image2 = image2;
+	}
+	public void setImage3(String image3) {
+		this.image3 = image3;
+	}
+	public void setImage4(String image4) {
+		this.image4 = image4;
+	}
+	public void setImage5(String image5) {
+		this.image5 = image5;
 	}
 }
